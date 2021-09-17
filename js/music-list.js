b73916cb3edec2622b2b6b1609af7934e47a4119
duonghieu147 <duonghieu4299@gmail.com>
2021-09-18 01:32:54 +0700

    Neumorphism Music Player

---
+++ js/music-list.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Xuất sơn ",
+    artist: "Hoa Chúc  Vương Thắng Nam",
+    img: "music-6",
+    src: "music-7"
+  },
+  {
+    name: "Mưa Bươm Bướm ",
+    artist: "Trương Lương Dĩnh ft Đặng Tử Kỳ",
+    img: "music-6",
+    src: "music-8"
+  },
+  {
+    name: "Tâm Lặng Như Nước ",
+    artist: " 心如止水 抖音音乐",
+    img: "music-6",
+    src: "music-9"
+  },
+  {
+    name: "Wrap Me In Plastic",
+    artist: " Wrap Me In Plastic",
+    img: "music-6",
+    src: "music-10"
+  },
+  {
+    name: "Sold Out",
+    artist: "Hawk Nelson Kimetsu",
+    img: "music-6",
+    src: "music-11"
+  },
+  {
+    name: "Believer ",
+    artist: "Dragons",
+    img: "music-6",
+    src: "music-12"
+  },
+  {
+    name: "Hiện Đại",
+    artist: "Khắc Việt",
+    img: "music-6",
+    src: "music-13"
+  },
+  {
+    name: "Yến Vô Hiết",
+    artist: "Châu Dương",
+    img: "music-14",
+    src: "music-14"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
